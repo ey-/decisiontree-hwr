056a9bd60bef88d9daf0892157349ee6248c8094
ey.engin.1991@gmail.com <ey.engin.1991@gmail.com@c04216f6-1023-bf1a-302d-5265993d0291>
2013-02-14 15:09:35 +0000

    Klasse zum Erkennen ob Attribut Diskret oder Stetig ist einfügen Implemended Issue 35.

---
+++ DecisionTree/Storage/TableData/CDataTypeChecker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecisionTree.Storage.TableData
+{
+    /// <summary>
+    /// Bestimmt AttributeDataType vom gegebenen Wert.
+    /// </summary>
+    public class CDataTypeChecker
+    {
+        
+        /*********************************************************************/
+        /// <summary>
+        /// setzt DataType
+        /// </summary>
+        /// <param name="value">CAttributeValue dessen Typ überprüft werden soll</param>
+        /// <param name="newValue">string der neue Wert</param>
+        public void handleDataTypeofValue(CAttributeValue value, string newValue)
+        {
+            value.AttributeType.DataType = checkDataType(newValue);
+        }
+
+        /*********************************************************************/
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">String dessen Datentyp bestimmt werden soll.</param>
+        /// <returns>E_DATATYPE der Datentyp des gegebenen Strings</returns>
+        protected E_DATATYPE checkDataType(string value)
+        {
+            int intvalue;
+            double doubleValue;
+
+
+            if (int.TryParse(value, out intvalue))
+            {
+                return E_DATATYPE.E_INT;
+            }
+            if (double.TryParse(value, out doubleValue))
+            {
+                return E_DATATYPE.E_FLOAT;
+            }
+            return E_DATATYPE.E_STRING;
+        }
+    }
+}
---
+++ DecisionTree/TestDecisionTree/CTestDataTypeChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecisionTree.Storage;
+using DecisionTree.Storage.TableData;
+
+namespace TestDecisionTree
+{
+    [TestClass]
+    public class CTestDataTypeChecker
+    {
+        [TestMethod]
+        public void TesthandleDataTypeofValue()
+        {
+            CAttributeType typeNumber = new CAttributeType(0);
+            typeNumber.setUsed("Number", E_DATATYPE.E_INT, true);
+
+            CAttributeValue myValue = new CAttributeValue(typeNumber, "1", "1", null);
+            CDataTypeChecker checker = new CDataTypeChecker();
+
+            checker.handleDataTypeofValue(myValue, "1.3");
+            Assert.IsTrue(myValue.AttributeType.DataType == E_DATATYPE.E_FLOAT, "float hinzufügen");
+            checker.handleDataTypeofValue(myValue, "2");
+            Assert.IsTrue(myValue.AttributeType.DataType == E_DATATYPE.E_INT, "int hinzufügen");
+            checker.handleDataTypeofValue(myValue, "wer");
+            Assert.IsTrue(myValue.AttributeType.DataType == E_DATATYPE.E_STRING, "string hinzufügen");
+        }
+    }
+}
